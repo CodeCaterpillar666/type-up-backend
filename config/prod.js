ba8d18efd8a57576979b92cef8bd091dfde16bef
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-06-15 14:20:12 -0700

    Fixed the mismatch for variables in config variables and prod.js

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  githubClientId: process.env.GOOGLE_CLIENT_ID,
-  githubClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET
 };
