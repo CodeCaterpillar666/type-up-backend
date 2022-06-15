e0c60ace8076a9720a44e2064d620a4a00d0f8bc
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-06-15 14:41:40 -0700

    Fixed the mismatch for variables in config variables and prod.js

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GithubStrategy = require("passport-github2").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 const keys = require('../config/keys');
 console.log(keys)
@@ -30,19 +29,6 @@ passport.use(
     }
   )
 );
-
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: FACEBOOK_APP_ID,
-//       clientSecret: FACEBOOK_APP_SECRET,
-//       callbackURL: "/auth/facebook/callback",
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       done(null, profile);
-//     }
-//   )
-// );
 
 passport.serializeUser((user, done) => {
   done(null, user);
